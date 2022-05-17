d6ce3540b98f16b0652a48f48a9e4765e10d8f4b
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2022-05-16 22:31:49 -0300

    created login page

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,7 +1,40 @@
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+// import { AuthContext } from "../../contexts/auth";
+import './SignIn.css';
+import logo from '../../assets/images/logo.png';
+// import { toast } from "react-toastify";
+
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // const { signIn, loadingAuth } = useContext(AuthContext);
+
+  function handleSubmit(e){
+    e.preventDefault();
+    // if(email !== '' && password !== '') signIn(email, password);
+    // if(email === '') toast.error('Insira o e-mail !');
+    // if(email !== '' && password === '') toast.error('Insira a senha !');
+  }
+
   return (
-    <div>
-      <h1>Tela de Login</h1>
+    <div className='container-center'>
+      <div className='login'>
+        <div className='logo-area'>
+          <img src={logo} alt='Logo do Sistema' />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input type='text' placeholder='email@email.com' value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type='password' placeholder='********' value={password} onChange={(e) => setPassword(e.target.value)} />
+          {/* <button type='submit'>{loadingAuth ? 'Carregando...' : 'Acessar'}</button> */}
+          <button type='submit'>Acessar</button>
+        </form>
+
+        <Link to='/register'>Criar uma conta</Link>
+
+      </div>
     </div>
   )
 }
