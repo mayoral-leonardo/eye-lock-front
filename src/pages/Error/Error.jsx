bca56a833920d1f226eadf9ee341cd5cb8ab2b94
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2022-05-22 21:07:38 -0300

    created error page

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,12 @@
+import './Error.css'
+import { Link } from 'react-router-dom'
+
+export default function Error () {
+  return (
+    <div className='not-found'>
+      <h1>404</h1>
+      <h2>Página não encontrada !</h2>
+      <Link to='/'>Veja todos os filmes </Link>
+    </div>
+  )
+} 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,15 @@ import { Switch } from "react-router-dom";
 import Route from "./Route";
 
 import SignIn from "../pages/SignIn/SignIn";
-
 import Dashboard from "../pages/Dashboard/Dashboard";
+import Error from "../pages/Error/Error";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={SignIn} />
       <Route exact path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="*" component={Error}/>
     </Switch>
   );
 }
