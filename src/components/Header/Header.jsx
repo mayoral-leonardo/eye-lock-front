4d6a4d9e074ff5d67b156af123282be7462cde40
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2022-05-25 22:23:59 -0300

    created and improved sidebar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,20 +3,12 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from "../../contexts/auth"
 import './Header.css';
 
-export default function Header({onClick}) {
+export default function Header({ onClick }) {
   const { user, signOut } = useContext(AuthContext);
   return (
     <header className='header'>
       <button className='header__items__buttons__item' onClick={onClick}>Eye Lock</button>
-
-      <div className='header__items'>
-        <div className='header__items__buttons'>
-          <Link className='header__items__buttons__item' to='/'>Usuários</Link>
-          <Link className='header__items__buttons__item' to='/'>Imagens</Link>
-          <Link className='header__items__buttons__item' to='/'>Funções</Link>
-        </div>
-        <button className='header__items__buttons__item' onClick={() => signOut()} >Sair</button>
-      </div>
+      <button className='header__items__buttons__item' onClick={() => signOut()} >Sair</button>
     </header>
   )
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,21 +7,23 @@ export default function Sidebar({ visible, onClose }) {
 
   const { lg } = useBreakpoint();
   return (
-    <Drawer
-      title="Menu"
-      placement="left"
-      closable
-      onClose={onClose}
-      visible={visible}
-      width={lg ? '30%' : '100%'}
-    >
-      <section className='sidebar'>
+    <section className='sidebar'>
+      <Drawer
+        drawerStyle={{backgroundColor: '#181C2E'}}
+        headerStyle={{backgroundColor: 'aqua'}}
+        title="Sistema"
+        placement="left"
+        closable
+        onClose={onClose}
+        visible={visible}
+        width={lg ? '12%' : '100%'}
+      >
         <div className='sidebar__main-content'>
-          <button>Usuários</button>
-          <button>Funções</button>
-          <button>Imagens</button>
+          <button className='sidebar__main-content__button'>Usuários</button>
+          <button className='sidebar__main-content__button'>Funções</button>
+          <button className='sidebar__main-content__button'>Imagens</button>
         </div>
-      </section>
-    </Drawer>
+      </Drawer>
+    </section>
   )
 }
